c780cf250cf92cffa68931b07fa0cd77c5a2d3e0
Javier Soto <javislop@gmail.com>
2021-01-24 02:28:52 -0600

    add configuration for cors and automapper

--- Startup.cs
+++ Startup.cs
@@ -1,15 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
 namespace fpAPI
@@ -32,6 +26,8 @@ namespace fpAPI
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "fpAPI", Version = "v1" });
             });
+            services.AddCors();
+            services.AddAutoMapper(typeof(Startup));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
