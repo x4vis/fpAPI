fcf283d9bb0a5f0af0741bbd78c94e02c7353586
Javier Soto <javislop@gmail.com>
2021-01-24 00:06:15 -0600

    add db context file

---
+++ Access/EFCtx.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace fpAPI.Access
+{
+    public static class EFCtx
+    {
+        public static DbCtx.InvCtx Inv;
+
+        public static void Init()
+        {
+            Inv = new DbCtx.InvCtx();
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -54,6 +54,8 @@ namespace fpAPI
             {
                 endpoints.MapControllers();
             });
+
+            Access.EFCtx.Init();
         }
     }
 }
